ecffcab996e9e5d46b553d97303dacb5d03cf547
jawil <609514766@qq.com>
2017-08-16 23:19:59 +0800

    增加点击选择时间

--- src/datePicker.js
+++ src/datePicker.js
@@ -3,7 +3,7 @@ module.exports = class datePicker {
         this.obj = obj
         this.index = -initIndex || 0
         this.callback = callback
-        this.deg = 25 //初始化偏转的角度
+        this.deg = 25 // 初始化偏转的角度
         this.length = this.obj.children.length
         this.distance = this.obj.children[0].offsetHeight
         this.ready()
@@ -12,23 +12,24 @@ module.exports = class datePicker {
         obj.style.webkitTransform = `translate3d(0,${dis}px,0)`
     }
     static setRotateX(obj, index, deg = 25) {
-        //设置每个Li的偏转角度
+        // 设置每个Li的偏转角度
         Array.prototype.slice.call(obj).forEach((ele, i) => {
             obj[i].style.webkitTransform = `rotateX(${(i+index)*deg}deg)`
         })
     }
     ready() {
-        //初始化运动距离
+        // 初始化运动距离
         datePicker.setTranslate3d(this.obj, this.index * this.distance)
-            //初始化3D偏转角度
+
+        // 初始化3D偏转角度
         datePicker.setRotateX(this.obj.children, this.index)
         this.bindEvent(this.obj)
     }
     bindEvent(selector) {
         let iStartPageY = 0,
-            step = 1, //弹性系数
+            step = 1, // 弹性系数
             prevPoint = 0,
-            speed = 0, //手指离开时候的瞬时速度,速度越大,最后停留的越远
+            speed = 0, // 手指离开时候的瞬时速度,速度越大,最后停留的越远
             timer = null,
             length = this.length - 1
 
@@ -46,10 +47,10 @@ module.exports = class datePicker {
             speed = (e.changedTouches[0].pageY - prevPoint)
             prevPoint = e.changedTouches[0].pageY
 
-            //已滑动在头部或尾部,但是用户还想往上或下滑,这是给一种越往上或下滑越难拖动的体验
+            // 已滑动在头部或尾部,但是用户还想往上或下滑,这是给一种越往上或下滑越难拖动的体验
             if ((this.index == 0 && iDisY > 0) || (this.index == -length && iDisY < 0)) {
-                step = 1 - Math.abs(iDisY) / selector.clientWidth //根据超出长度计算系数大小，超出的越到 系数越小
-                step = Math.max(step, 0) //系数最小值为0
+                step = 1 - Math.abs(iDisY) / selector.clientWidth // 根据超出长度计算系数大小，超出的越到 系数越小
+                step = Math.max(step, 0) // 系数最小值为0
                 iDisY = parseInt(iDisY * step)
             }
             datePicker.setTranslate3d(selector, this.index * this.distance + iDisY)
@@ -87,7 +88,7 @@ module.exports = class datePicker {
                         this.index = this.index > 0 ? 0 : (this.index < -length ? -length : this.index)
                         selector.style.webkitTransitionDuration = '400ms'
                         selector.addEventListener("webkitTransitionEnd", f => {
-                            //touchend事件触发后会有一个动画，触发完成后立即清除transition
+                            // touchend事件触发后会有一个动画，触发完成后立即清除transition
                             selector.style.webkitTransitionDuration = '0ms'
                         })
                         Array.prototype.slice.call(selector.children).forEach(ele => {
